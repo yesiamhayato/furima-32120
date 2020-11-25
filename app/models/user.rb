90618195a6722784c8529d10123a87c56e2aee93
yesiamhayato <njusa18endo@gmail.com>
2020-11-25 15:59:20 +0900

    userモデルemail一意性validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :email, uniqueness: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
