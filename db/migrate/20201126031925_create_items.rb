4742abeede82be7e3cbe71f5b07f5ca38e7b0469
yesiamhayato <njusa18endo@gmail.com>
2020-11-26 12:50:43 +0900

    itemsテーブル作成

--- db/migrate/20201126031925_create_items.rb
+++ db/migrate/20201126031925_create_items.rb
@@ -1,8 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
-
+      t.string     :item_name,          null: false
+      t.text       :item_info,          null: false
+      t.integer    :category_id,        null: false
+      t.integer    :price,              null: false
+      t.integer    :condition_id,       null: false
+      t.integer    :delivery_fee,       null: false
+      t.integer    :shipped_from_id,    null: false
+      t.integer    :until_shipped_id,   null: false
+      t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_081710) do
+ActiveRecord::Schema.define(version: 2020_11_26_031925) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_info", null: false
+    t.integer "category_id", null: false
+    t.integer "price", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "shipped_from_id", null: false
+    t.integer "until_shipped_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_11_24_081710) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
