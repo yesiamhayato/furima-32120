430dca024b25c740fabbd733c6ea3097e025089e
yesiamhayato <njusa18endo@gmail.com>
2020-11-26 18:15:11 +0900

    activehashのconditionモデル追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_info
-    validates :condition_id
     validates :delivery_fee_id
     validates :shipped_from_fee_id
     validates :until_shipped_id
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
+      validates :condition_id
     end
 
     with_options format: { with: /\A[0-9]+\z/ } do
@@ -23,5 +23,6 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 
 end
