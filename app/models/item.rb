31552705b783a12cf9982cb1843695011fc79d81
yesiamhayato <njusa18endo@gmail.com>
2020-11-26 15:55:56 +0900

    itemモデルのバリデーション設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :shipped_from_fee_id
+    validates :until_shipped_id
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      with_options　numerically: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999} do
+        validates :price
+      end
+    end
+  end
+
   belongs_to :user
 end
