3f6517679deb37a62d80dcf0c7d019a36122f342
yesiamhayato <njusa18endo@gmail.com>
2020-11-26 18:06:17 +0900

    activehashのcategoryモデル追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,17 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_info
-    validates :category_id
     validates :condition_id
     validates :delivery_fee_id
     validates :shipped_from_fee_id
     validates :until_shipped_id
 
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+    end
+
     with_options format: { with: /\A[0-9]+\z/ } do
-      with_options　numerically: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999} do
+      with_options　numerically: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999 } do
         validates :price
       end
     end
@@ -17,4 +20,8 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
 end
