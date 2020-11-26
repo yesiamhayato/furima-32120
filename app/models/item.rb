15baa093cd239e5f74a81bb31b0c547b5a5d2c5f
yesiamhayato <njusa18endo@gmail.com>
2020-11-26 20:21:48 +0900

    imageのバリデーションitemモデルにて

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
   with_options presence: true do
     validates :item_name
     validates :item_info
+    validates :image
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
@@ -17,9 +21,6 @@ class Item < ApplicationRecord
       end
     end
   end
-
-  belongs_to :user
-  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
