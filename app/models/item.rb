a1226f201047087aa51c8f30df23e37bd450caf8
yesiamhayato <njusa18endo@gmail.com>
2020-11-29 21:22:37 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :shipped_from
+  belongs_to :until_shipped
+
   with_options presence: true do
     validates :item_name
     validates :item_info
@@ -18,11 +25,4 @@ class Item < ApplicationRecord
 
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :delivery_fee
-  belongs_to :shipped_from
-  belongs_to :until_shipped
 end
