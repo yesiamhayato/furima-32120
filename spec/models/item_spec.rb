96591b62d11561e0d6f1027ad0cbc0b7bdf0cd48
yesiamhayato <njusa18endo@gmail.com>
2020-11-29 19:42:18 +0900

    priceは299円以下では登録できない priceは10_000_000以上では登録できないをテストコードに追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,6 +63,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it 'priceは299円以下では登録できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceは10_000_000以上では登録できない' do
+        @item.price = 999999999999
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
     end
   end
 end
