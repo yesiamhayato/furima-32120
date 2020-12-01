d0cca18bdbcf8a141c8b262b18d0a8f115c0a89e
yesiamhayato <njusa18endo@gmail.com>
2020-12-01 11:58:05 +0900

    updateアクションに@itemを定義(エラーメッセージのテンプレがrenderされた時にエラーが生じるため)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
     item = Item.find(params[:id])
     if item.update(item_params)
       redirect_to item_path
