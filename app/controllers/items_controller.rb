fcf3f58a823a5cd496e43a983fdc3698c5b816f3
yesiamhayato <njusa18endo@gmail.com>
2020-12-01 12:04:37 +0900

    updateでもエラーメッセージが表示するようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
