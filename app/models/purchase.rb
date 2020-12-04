3a81664c3c9d16f137de83cd0797b39e759e22e6
yesiamhayato <njusa18endo@gmail.com>
2020-12-04 14:39:29 +0900

    purchaseモデルのattr_accessor記入

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase
   include ActiveModel::Model
-  attr_accessor 
+  attr_accessor :number, :cvc, :exp_month, :exp_year, :user_id, :item_id, :zip_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   # ここにバリデーションの処理を書く
 
