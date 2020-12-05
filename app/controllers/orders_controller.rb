7e07b6e44aed3bc1d2e54b31ac3addfa6fc66008
yesiamhayato <njusa18endo@gmail.com>
2020-12-05 11:36:51 +0900

    formオブジェクト編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
   end
 
   def create
-
+    @purchase = Purchase.new(order_params)
   end
 
   private
   def order_params
-
+    params.permit( :zip_code, :prefecture_id, :city, :house_number, :building_number )
   end
 
 end
