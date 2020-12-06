d61bbc3718081fb9ce53b68742d7f72e6b8e1a6d
yesiamhayato <njusa18endo@gmail.com>
2020-12-06 15:46:41 +0900

    Update purchase.rb

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,8 +3,6 @@ class Purchase
   attr_accessor :token, :user_id, :item_id, :zip_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
   with_options presence: true do
     validates :token
-    #validates :user_id
-    #validates :item_id
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
