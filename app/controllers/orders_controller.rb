1b188be4f47301561f14a9b1bc9b1eb0595ecb7b
yesiamhayato <njusa18endo@gmail.com>
2020-12-06 14:08:35 +0900

    payjp機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_params, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @purchase = Purchase.new
   end
 
   def create
-    @purchase = Purchase.new(order_params)
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
-  def order_params
-    params.permit( :zip_code, :prefecture_id, :city, :house_number, :building_number )
+
+  def item_params
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def pay_item
+    Payjp.api_key = "sk_test_475f6015b8c25ad822ce9133"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+  end
+
+  def purchase_params
+    params.require(:purchase).permit( :zip_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id).merge( token: params[:token], user_id: current_user.id)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,12 @@
 class Order < ApplicationRecord
   attr_accessor :token
+  
+  validates :token, presence: true
 
   belongs_to :item
   belongs_to :user
   has_one :address
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipped_from
 end
