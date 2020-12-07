db72ccd47252849300b3b1eb225502174bb9be26
yesiamhayato <njusa18endo@gmail.com>
2020-12-07 20:38:19 +0900

    payjpの秘密鍵を環境変数から呼び出す記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = 'sk_test_475f6015b8c25ad822ce9133'  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       card: purchase_params[:token], # カードトークン
