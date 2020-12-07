35a8cbe95096e05d7ac521434064123725748ead
yesiamhayato <njusa18endo@gmail.com>
2020-12-07 20:40:38 +0900

    フォームオブジェクト内のwith_options presence: true do内に、user_idとitem_idも追加

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -9,6 +9,8 @@ class Purchase
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
   end
 
   def save
